d45e5345991b1c530608d597618193e4a8ff33f1
smayranger-coder <smayranger@gmail.com>
2026-07-12 02:13:38 +0700

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  themeColor: "#194536",
 };
 
 export default function RootLayout({ children }) {
